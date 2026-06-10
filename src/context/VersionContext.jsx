ca7debae65616f3a40da6b54545091bd7d31fccd
LuisSG9915 <luis.sg9915@gmail.com>
2026-06-09 18:30:56 -0600

    versionador

---
+++ src/context/VersionContext.jsx
@@ -0,0 +1,57 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
+
+const VersionContext = createContext();
+
+export const useVersion = () => {
+  const context = useContext(VersionContext);
+  if (!context) {
+    throw new Error("useVersion must be used within a VersionProvider");
+  }
+  return context;
+};
+
+export const VersionProvider = ({ children }) => {
+  const CURRENT_VERSION = 18;
+  const [verificadorVersion, setVerificadorVersion] = useState(false);
+
+  const checkVersion = async () => {
+    try {
+      const res = await axios.get("https://api.cbinformatica.net:9072/versionPEINADOSAGENDA");
+      if (res.data.ver > CURRENT_VERSION) {
+        Swal.fire({
+          title: "Actualización",
+          text: `Favor de actualizar sitio, hemos detectado una versión anterior`,
+          icon: "info",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Actualizar",
+          allowOutsideClick: false,
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.reload();
+          }
+        });
+        setVerificadorVersion(true);
+      } else {
+        setVerificadorVersion(false);
+      }
+    } catch (error) {
+      console.error("Error checking version:", error);
+    }
+  };
+
+  useEffect(() => {
+    checkVersion();
+    const interval = setInterval(checkVersion, 60000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <VersionContext.Provider value={{ CURRENT_VERSION, verificadorVersion, checkVersion }}>
+      {children}
+    </VersionContext.Provider>
+  );
+};
--- src/examples/index.jsx
+++ src/examples/index.jsx
@@ -4,21 +4,24 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Example from "./Basic";
 import "../css/style.css";
 import { AuthProvider } from "../context/AuthContext";
+import { VersionProvider } from "../context/VersionContext";
 import CrearCita from "../screens/CrearCita";
 import ListaEspera from "../screens/ListaEspera";
 import EditarCita from "../screens/EditarCita";
 import Agenda2 from "../screens/Agenda2";
 createRoot(document.getElementById("app")).render(
-  <AuthProvider>
-    {/* <Example /> */}
-    <Router>
-      <Routes>
-        <Route path="/" element={<Example />} />
-        <Route path="/miliga/crearcita" element={<CrearCita />} />
-        <Route path="/miliga/listaEspera" element={<ListaEspera />} />
-        <Route path="/miliga/editarcita" element={<EditarCita />} />
-        <Route path="/miliga/Agenda2" element={<Agenda2 />} />
-      </Routes>
-    </Router>
-  </AuthProvider>
+  <VersionProvider>
+    <AuthProvider>
+      {/* <Example /> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Example />} />
+          <Route path="/miliga/crearcita" element={<CrearCita />} />
+          <Route path="/miliga/listaEspera" element={<ListaEspera />} />
+          <Route path="/miliga/editarcita" element={<EditarCita />} />
+          <Route path="/miliga/Agenda2" element={<Agenda2 />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  </VersionProvider>
 );
